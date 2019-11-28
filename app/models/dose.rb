202e17cdb96575e6aa83951d0644ff51dc6f01cc
Kevin Liebholz <kevin@liebholz.eu>
2019-11-28 13:09:22 +0100

    model validations Dose Cocktail Ingredient

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true, allow_blank: false
+  validates :cocktail, presence: true, allow_blank: false
+  validates :ingredient, presence: true, allow_blank: false
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
